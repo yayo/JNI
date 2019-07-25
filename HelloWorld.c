f11a269d199e3a7d5677dbc79fe69df5de55120a
Robert Young <yayooo@gmail.com>
2019-07-25 19:26:06 +0800

    3In 1Out

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,10 +1,27 @@
 
 #include <jni.h>
 #include <stdio.h>
+#include <assert.h>
 #include "HelloWorld.h"
  
-JNIEXPORT jlong JNICALL Java_HelloWorld_sayHello (JNIEnv *env, jobject thisObj)
+JNIEXPORT jlongArray JNICALL Java_HelloWorld_sayHello (JNIEnv *env, jobject thisObj,jlong i1,jlong i2,jlong i3)
  {
   printf("Hello, Wor2d\n");
-  return(1);
+  const size_t n=4;
+  assert(8==sizeof(i1));
+  jlongArray out = (*env)->NewLongArray(env, n);
+  if(NULL==out)
+   {
+    fprintf(stderr,"Out of Memory!");
+   }
+  else
+   {jlong buf[n];
+    int i;
+    for(i=0;i<n;i++)
+     {
+      buf[i]=70+i;
+     }
+    (*env)->SetLongArrayRegion(env, out, 0, n, buf);
+   }
+  return(out);
  }
--- HelloWorld.java
+++ HelloWorld.java
@@ -6,13 +6,13 @@ public class HelloWorld
    {
     System.loadLibrary("hello"); 
    }
-  private native long sayHello();
+  private native long[] sayHello(long i1,long i2,long i3);
    // Exception in thread "main" java.lang.UnsatisfiedLinkError: no hello in java.library.path
    // Exception in thread "main" java.lang.UnsatisfiedLinkError: HelloWorld.sayHello()V
   public static void main(String[] args)
    {
     System.out.println(s);
-    long out1=new HelloWorld().sayHello();
-    System.out.println(""+out1);
+    long result[]=new HelloWorld().sayHello(2,3,4);
+    System.out.println(""+(result[0]+result[1]+result[2]+result[3]));
    }
  }
